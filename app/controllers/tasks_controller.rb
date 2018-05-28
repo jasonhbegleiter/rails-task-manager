6e40239a46bc57bc565fd159c8cb08ff33529b3a
Jason Begleiter <jhbegs204@gmail.com>
2018-05-28 13:07:57 +0300

    Details working, but not pretty

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,8 @@ class TasksController < ApplicationController
   def list
     @tasks = Task.all
   end
+
+  def details
+    @task = Task.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'list', to: 'tasks#list', as: :list
+  get 'list/:id', to: 'tasks#details', as: :details
 end
