7f1ec81ef4116d2ec459bba6ed5303cd0611d5ba
Jason Begleiter <jhbegs204@gmail.com>
2018-05-28 13:39:00 +0300

    Add new task is working, but ugly

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'list', to: 'tasks#list', as: :list
   get 'list/:id', to: 'tasks#details', as: :details
+  get 'new', to: 'tasks#new', as: :new
+  post 'new', to: 'tasks#create', as: :create
 end
